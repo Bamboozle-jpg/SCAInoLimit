f93dd8438fdfce0fc5719383b3c02921bdf501dc
Bamboozle-jpg <kevin@bachelorclan.com>
2022-11-27 00:54:21 -0800

    wrote training function and setup for my data

--- Model/model.py
+++ Model/model.py
@@ -1,4 +1,8 @@
 import torch
+import csv
+import pandas as pd
+import numpy as np
+from torch.utils.data import random_split, TensorDataset, DataLoader
 
 class testModel(torch.nn.Module):
     # Initialize model
@@ -32,39 +36,122 @@ class testModel(torch.nn.Module):
         model.eval()
         return model
 
-# Setup Linear transformation and tensor to test it with
-lin = torch.nn.Linear(3, 2)
-x = torch.rand(1, 3)
-print('Input:', x)
-print('\nWeight and Bias parameters:')
-for param in lin.parameters():
-    print(param)
+    # Function for training the model
+    def trainn(self, numEpochs, trainLoader, validateLoader):
+        lossFn = torch.nn.CrossEntropyLoss()
+        optimizer = torch.optim.Adam(self.parameters(), lr=0.001, weight_decay=0.0001)
+        bestAccuracy = 0.0
+        
+        print("Training with", numEpochs, "epochs...")
+        for epoch in range(1, numEpochs + 1):
+            # For each epoch resets epoch vars
+            runningTrainingLoss = 0.0
+            runningAccuracy = 0.0
+            runningValLoss = 0.0
+            total = 0
 
-# Testing a linear transformation
-y = lin(x)
-print('\nOutput:')
-print(y)
+            # Actually trains
+            for data in trainLoader:
+                inputs, outputs = data
+                outputs = outputs.long()
+                # Zero param gradients
+                optimizer.zero_grad()
+                predictedOutputs = self.forward(inputs)
+                # Sets up and uses backpropogation to optimize
+                trainLoss = lossFn(predictedOutputs, outputs[:, 0])
+                trainLoss.backward()
+                optimizer.step()
+                runningTrainingLoss += trainLoss.item()
 
-# Create random tensor
-x = torch.rand(1, 100)
+            trainLossValue = runningTrainingLoss/len(trainLoader)
 
-# Creates model and tells it to use cpu
-exModel = testModel(100, 1)
-device = torch.device("cpu")
-exModel.to(device)
+            # Validation (AKA Figure out which model change was the best)
+            with torch.no_grad():
+                self.eval()
+                for data in validateLoader:
+                    inputs, outputs = data
+                    outputs = outputs.long()
+                    # Gets values for loss
+                    predictedOutputs = self(inputs)
+                    valLoss = lossFn(predictedOutputs, outputs[:, 0])
+                    # Highest value will be our prediction
+                    _, predicted = torch.max(predictedOutputs, 1)
+                    runningValLoss += valLoss.item()
+                    total += outputs.size(0)
+                    runningAccuracy += (predicted == outputs).sum().item()
+            
+            # Calculate Validation Loss Val
+            valLossValue = runningValLoss/len(validateLoader)
+            # Accuracy = num of correct predictions in validation batch / total predictions done
+            accuracy = (100 * runningAccuracy / total)
 
-# Tests Model
-y = exModel.forward(x)
-print(y)
+            # Save model if accuracy is best
+            if accuracy > bestAccuracy:
+                self.saveModel("testModel.pth")
+                bestAccuracy = accuracy
 
-# Saves Model
-exModel.saveModel("testModel.pth")
+            # Print current Epoch stats
+            print("Completed training for epoch :", epoch, 'Training Loss is %.4f' %trainLossValue, 'Validation Loss is: %.4f' %valLossValue, 'Accuracy is %d %%' % (accuracy))
 
-#Loads model and tests it
-model2 = testModel.loadModel(100, 1, "testModel.pth")
-y = model2.forward(x)
-print(y)
 
-# Loss function and optimizer
-loss_fn = torch.nn.CrossEntropyLoss()
-optimizer = torch.optim.Adam(model2.parameters(), lr=0.001, weight_decay=0.0001)
+# Grabs data and turns it into usable form:
+
+# Grabs raw list of dictionaries
+everythingDict=[*csv.DictReader(open('finalData.csv'))]
+actualDict = {}
+actualDict['solov'] = []
+actualDict['height'] = []
+actualDict['inundate'] = []
+
+# Turns list of dictionaries into dictionary of lists
+for i in range(0, len(everythingDict)):
+    actualDict['solov'].append(float(everythingDict[i]['solovievIdentity']))
+    actualDict['height'].append(float(everythingDict[i]['waveHeight']))
+    actualDict['inundate'].append(float(everythingDict[i]['horizontalInundation']))
+
+# Creates Pandas dataframe for input and output
+df = pd.DataFrame(actualDict)
+input = df.loc[:, ['height', 'inundate']]
+output = df.loc[:, ['solov']]
+
+# Turns pandas dataframes into tensors and Tensor Dataset
+input = torch.Tensor(input.to_numpy())
+output = torch.Tensor(output.to_numpy())
+data = TensorDataset(input, output)
+
+# Split into a training, validation and testing set
+trainBatchSize = 10
+testSplit = int(len(input)*0.25)
+print(testSplit)
+trainSplit = int(len(input)*0.6)
+print(trainSplit)
+validateSplit = len(input) - trainSplit - testSplit
+print(validateSplit)
+print(len(input))
+trainSet, validateSet, testSet = random_split(data, [trainSplit, validateSplit, testSplit])
+
+# Get data in loadable form to go into model
+trainLoader = DataLoader(trainSet, batch_size=trainBatchSize, shuffle=True)
+validateLoader = DataLoader(validateSet, batch_size=1)
+testLoader = DataLoader(testSet, batch_size=1)
+
+# Sets input and output size for future models
+inputSize = list(input.shape)[1]
+solovs = []
+for i in actualDict['solov']:
+    if not i in solovs:
+        solovs.append(i)
+print(solovs)
+outputSize = len(solovs)
+
+
+# TRAINING AND TESTING MODEL!!!
+
+# Actually put it into the model
+# For loading current one
+waveModel = testModel.loadModel(inputSize, outputSize, "testModel.pth")
+# For creating new one
+# waveModel = testModel(inputSize, outputSize)
+print("input size :", inputSize)
+print("output size :", outputSize)
+waveModel.trainn(10, trainLoader, validateLoader)
